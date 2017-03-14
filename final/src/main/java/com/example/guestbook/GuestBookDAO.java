53df1aacd896a5bdfe24d76683d789ef66d20880
Nexome <nicolas.latcher@gmail.com>
2017-03-14 15:13:37 +0100

    Modification DAO : paramètre string au lieu de int

--- final/src/main/java/com/example/guestbook/GuestBookDAO.java
+++ final/src/main/java/com/example/guestbook/GuestBookDAO.java
@@ -110,8 +110,8 @@ public class GuestBookDAO
         ResultSet resultSet = statement.executeQuery(sql);
         while (resultSet.next())
         {
-            String book = resultSet.getString(1);
-            Integer nb = resultSet.getInt(2);
+            String book = resultSet.getString("book");
+            Integer nb = resultSet.getInt("nb");
             resultat.put(book, nb);
         }
         return resultat;
